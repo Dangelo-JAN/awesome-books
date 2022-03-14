707d153ab0035f83909f0d75236d91b05e2210e4
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-14 15:17:54 -0400

    Setup Project

---
+++ app.js
@@ -0,0 +1,30 @@
+const bookSection = document.getElementById(book-sectin);
+const addBook = document.getElementById(add);
+const bookList = [
+  {
+    name: 'The Red Thin Line',
+    autor: 'James Jones',
+  },
+  {
+    name: 'The Green Mile',
+    autor: 'Stephen King',
+  },
+  {
+    name: 'Batman The Dark Knight',
+    autor: 'Chritopher Nolan, Johathan',
+  },
+  {
+    name: ,
+    autor: ,
+  },
+  {
+    name: ,
+    autor: ,
+  },
+  {
+    name: ,
+    autor: ,
+  },
+];
+
+addBook.addEventListener()
