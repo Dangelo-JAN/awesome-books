f258cc4da89ab6e0be6c1af5e9585f80900cf97b
fasaldana <fernando_s_98@hotmail.com>
2022-03-15 09:45:14 -0500

    Added function for adding a new book

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
 const bookSection = document.getElementById('book-section');
 const addBook = document.getElementById('add');
-const bookList = [{
-}];
+document.getElementById("form").addEventListener("submit", add);
 
-var names = [];
-var authors = [];
+function add(e){
+  name = document.getElementById("input-name").value;
+  author = document.getElementById("input-author").value;
 
-for(let i = 0; i < bookList.length; i++){
-  const div = document.createElement('div');
-  div.classList.add('book-list');
-  const bookName = document.createElement('span');
-  bookName.classList.add('book-name');
-  const bookAuthor = document.createElement('p');
-  bookAuthor.classList.add('book-author');
-  const removeBtn = document.createElement('button');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.textContent = 'Remove';
-  const divider = document.createElement('hr');
+  let book = {
+    name,
+    author
+  }
 
-  bookName.textContent = names[i];
-  bookAuthor.textContent = authors[i];
-  div.append(bookName, bookAuthor, removeBtn);
-  bookSection.append(div, divider);
-
-  names.push(bookList[i].name);
-  authors.push(bookList[i].author)
+  if(localStorage.getItem("books") === null){
+    let books = [];
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  } else {
+    let books = JSON.parse(localStorage.getItem("books"));
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  document.getElementById("form").reset();
+  e.preventDefault();
+  window.location.reload();
 }
 
-console.log(localStorage)
+function loadScreen(){
+  let books = JSON.parse(localStorage.getItem("books"));
+  for(let i = 0; i < books.length; i++){
+    let name = books[i].name;
+    let author = books[i].author;
+    const div = document.createElement('div');
+    div.classList.add('book-list');
+    const bookName = document.createElement('span');
+    bookName.classList.add('book-name');
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-author');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = 'Remove';
+    const divider = document.createElement('hr');
 
+    bookName.textContent = name;
+    bookAuthor.textContent = author;
+    div.append(bookName, bookAuthor, removeBtn);
+    bookSection.append(div, divider);
+  }
+}
 
-addBook.addEventListener('click', () => {
-  const inputName = document.getElementById('input-name').value;
-  const inputAuthor = document.getElementById('input-author').value;
-  const obj = {};
-  obj['name'] = inputName;
-  obj['author'] = inputAuthor;
-  bookList.push(obj);
-  names.push(inputName);
-  authors.push(inputAuthor);
-  localStorage.setItem('name', names);
-  localStorage.setItem('author', authors);
-  console.log(bookList);
-})
+loadScreen();
