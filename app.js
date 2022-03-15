3ca8e49fdf3d65fd740afebbcb586e52102f4b85
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-15 17:14:13 -0400

    Create classes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const bookSection = document.getElementById('book-section');
 
-function add(e) {
+/*function add(e) {
   const name = document.getElementById('input-name').value;
   const author = document.getElementById('input-author').value;
   const books = [];
@@ -23,11 +23,11 @@ function add(e) {
   document.getElementById('form').reset();
   e.preventDefault();
   window.location.reload();
-}
+}*/
 
 document.getElementById('form').addEventListener('submit', add);
 
-function remove(id) {
+/*function remove(id) {
   const books = JSON.parse(localStorage.getItem('books'));
 
   for (let i = 0; i < books.length; i += 1) {
@@ -38,6 +38,75 @@ function remove(id) {
 
   localStorage.setItem('books', JSON.stringify(books));
   window.location.reload();
+}*/
+
+/*function loadScreen() {
+  const books = JSON.parse(localStorage.getItem('books'));
+  for (let i = 0; i < books.length; i += 1) {
+    const { id } = books[i];
+    const { name } = books[i];
+    const { author } = books[i];
+    const div = document.createElement('div');
+    div.classList.add('book-list');
+    const bookName = document.createElement('span');
+    bookName.classList.add('book-name');
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-author');
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = 'Remove';
+    removeBtn.onclick = function RemoveBtn() {
+      remove(id);
+    };
+    const divider = document.createElement('hr');
+
+    bookName.textContent = name;
+    bookAuthor.textContent = author;
+    div.append(bookName, bookAuthor, removeBtn);
+    bookSection.append(div, divider);
+  }
+}*/
+
+loadScreen();
+
+class Book {
+  constructor(name, author) {
+    this.id = null;
+    this.name = name;
+    this.author = author;
+  }
+
+  add(book) {
+    const name = document.getElementById('input-name').value;
+    const author = document.getElementById('input-author').value;
+    const books = [];
+  
+    if (localStorage.getItem('books') === null) {
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    } else {
+      const books = JSON.parse(localStorage.getItem('books'));
+      book.id = books.length;
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+    document.getElementById('form').reset();
+    e.preventDefault();
+    window.location.reload();
+  }
+
+  remove(id) {
+    const books = JSON.parse(localStorage.getItem('books'));
+  
+    for (let i = 0; i < books.length; i += 1) {
+      if (id === books[i].id) {
+        books.splice(i, 1);
+      }
+    }
+  
+    localStorage.setItem('books', JSON.stringify(books));
+    window.location.reload();
+  }
 }
 
 function loadScreen() {
@@ -67,4 +136,42 @@ function loadScreen() {
   }
 }
 
-loadScreen();
+/*class methods {
+  constructor() {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+  }
+
+  add(book) {
+    const name = document.getElementById('input-name').value;
+    const author = document.getElementById('input-author').value;
+    const books = [];
+  
+    if (localStorage.getItem('books') === null) {
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    } else {
+      const books = JSON.parse(localStorage.getItem('books'));
+      book.id = books.length;
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+    document.getElementById('form').reset();
+    e.preventDefault();
+    window.location.reload();
+  }
+
+  remove(id) {
+    const books = JSON.parse(localStorage.getItem('books'));
+  
+    for (let i = 0; i < books.length; i += 1) {
+      if (id === books[i].id) {
+        books.splice(i, 1);
+      }
+    }
+  
+    localStorage.setItem('books', JSON.stringify(books));
+    window.location.reload();
+  }
+}*/
