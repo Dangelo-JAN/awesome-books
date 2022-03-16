6db8e5abb78861427bbbb4055fca22c2bbf1d990
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-16 18:05:59 -0400

    Fix linters

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-class Book {
-  id;
-  name;
-  author;
-}
-
 class BookList {
-  bookSection = document.getElementById("book-section");
-  addBtn = document.getElementById("add");
+  bookSection = document.getElementById('book-section');
 
-  createEmpty() {
+  addBtn = document.getElementById('add');
+
+  createEmpty = () => {
     const books = [];
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
-  add(e) {
-    const name = document.getElementById("input-name").value;
-    const author = document.getElementById("input-author").value;
+  add = (e) => {
+    const name = document.getElementById('input-name').value;
+    const author = document.getElementById('input-author').value;
     const books = [];
 
     const book = {
@@ -24,34 +19,34 @@ class BookList {
       author,
     };
 
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       books.push(book);
-      localStorage.setItem("books", JSON.stringify(books));
+      localStorage.setItem('books', JSON.stringify(books));
     } else {
-      const books = JSON.parse(localStorage.getItem("books"));
+      const books = JSON.parse(localStorage.getItem('books'));
       book.id = books.length;
       books.push(book);
-      localStorage.setItem("books", JSON.stringify(books));
+      localStorage.setItem('books', JSON.stringify(books));
     }
-    document.getElementById("form").reset();
+    document.getElementById('form').reset();
     e.preventDefault();
     window.location.reload();
   }
 
-  remove(id) {
-    const books = JSON.parse(localStorage.getItem("books"));
+  remove = (id) => {
+    const books = JSON.parse(localStorage.getItem('books'));
     for (let i = 0; i < books.length; i += 1) {
       if (id === books[i].id) {
         books.splice(i, 1);
       }
     }
 
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     window.location.reload();
   }
 
   loadScreen() {
-    const books = JSON.parse(localStorage.getItem("books"));
+    const books = JSON.parse(localStorage.getItem('books'));
     if (!books) {
       this.createEmpty();
     }
@@ -59,23 +54,23 @@ class BookList {
       const { id } = books[i];
       const { name } = books[i];
       const { author } = books[i];
-      const div = document.createElement("div");
-      div.classList.add("book-list");
-      const bookName = document.createElement("p");
-      bookName.classList.add("book-name");
-      const bookAuthor = document.createElement("p");
-      bookAuthor.classList.add("book-author");
-      const divBtn = document.createElement("div");
-      divBtn.classList.add("btn-container");
-      const removeBtn = document.createElement("button");
-      removeBtn.classList.add("remove-btn");
-      removeBtn.textContent = "Remove";
-      removeBtn.addEventListener("click", () => {
+      const div = document.createElement('div');
+      div.classList.add('book-list');
+      const bookName = document.createElement('p');
+      bookName.classList.add('book-name');
+      const bookAuthor = document.createElement('p');
+      bookAuthor.classList.add('book-author');
+      const divBtn = document.createElement('div');
+      divBtn.classList.add('btn-container');
+      const removeBtn = document.createElement('button');
+      removeBtn.classList.add('remove-btn');
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', () => {
         this.remove(id);
       });
       divBtn.append(removeBtn);
 
-      bookName.textContent = '"' + name + '"' + " by";
+      bookName.textContent = `"${name}" by`;
       bookAuthor.textContent = author;
       div.append(bookName, bookAuthor, divBtn);
       this.bookSection.append(div);
@@ -84,5 +79,5 @@ class BookList {
 }
 
 const book = new BookList();
-document.getElementById("form").addEventListener("submit", book.add);
+document.getElementById('form').addEventListener('submit', book.add);
 book.loadScreen();
